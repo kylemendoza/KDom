d18351f5b0e7ef310c18a536f6823b04014b98cb
kylemendoza <58937615+kylemendoza@users.noreply.github.com>
2019-12-16 18:42:56 +0800

    Add files via upload

---
+++ CALCULATOR CS.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            double firstnum;
+            double secondnum;
+            double answer;
+            string ops;
+
+            Console.WriteLine("\t\t\tSimple Calculator");
+
+            Console.WriteLine("\t\t\tEnter First Number: ");
+            firstnum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\tSelect an Operator: ");
+            ops = Console.ReadLine();
+
+            Console.WriteLine("\t\t\tEnter Second Number: ");
+            secondnum = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("---------------------");
+
+            if (ops == "+")
+            {
+                answer = firstnum + secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "-")
+            {
+                answer = firstnum - secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "/")
+            {
+                answer = firstnum / secondnum;
+                Console.Write(answer);
+            }
+            if (ops == "*")
+            {
+                answer = firstnum * secondnum;
+                Console.Write(answer);
+
+            }
+
+            
+            Console.ReadKey();
+    
+            
+        }
+    }
+}
